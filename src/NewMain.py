56a3f8604e8fbcba69400627857abbf7ec9d6b39
Akshay Kumar Singh <kakshay25540@gmail.com>
2024-12-20 23:43:56 +0530

    PEN-21740 | added class for test
    fix
    
    fix

---
+++ src/NewMain.py
@@ -0,0 +1,24 @@
+import json
+
+def load_config(file_path):
+    try:
+        with open(file_path) as f:
+            data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        data = {"a": 10, "b": 2}
+    return data
+
+def calculate(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero, defaulting result to 0")
+        return 0
+
+def main():
+    config = load_config("config.json")
+    result = calculate(config.get("a", 1), config.get("b", 1))
+    print("Result:", result)
+
+if __name__ == "__main__":
+    main()
---
+++ src/main.py
@@ -0,0 +1,20 @@
+import json
+
+def load_config(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+    return data
+
+def calculate(a, b):
+    if(b != 0):
+        print("Error: Division by zero, defaulting result by 0")
+        return 0
+    return a / b
+
+def main():
+    config = load_config("config.json")
+    result = calculate(config["a"], config["b"])
+    print("Result:", result)
+
+if __name__ == "__main__":
+    main()
